77947bad4b336d17bbc851bfc9630070a15caad5
huongnguyen2152360 <huongnguyen2152360@gmail.com>
2018-09-27 09:32:53 +0700

    done all

--- learntosocketio/bin/socket.js
+++ learntosocketio/bin/socket.js
@@ -3,10 +3,24 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
 io.on("connection", function(socket) {
-    console.log("A user connected, id = " + socket.id);
+  console.log(socket.id + " is connected.");
+  socket.on("disconnect", function() {
+    console.log(socket.id + " is disconnected.");
+  });
+  socket.on("Client-send-data", function(msg){
+      console.log(msg);
+      // Case 1: Server sends to all windows
+    //   io.sockets.emit("Server-send-data", "server sending to other ones: " + msg)
+      
+    // Case 2: Server sends to A only
+    // socket.emit("Server-send-data", "server sending to A only: " + msg)
     
+    // Case 3: A clicks, server sends to B, C // B clicks, server sends to A,C
+    socket.broadcast.emit("Server-send-data", "server not send to A: " + msg)
+
+  })
 });
 
 http.listen(5000, function() {
-    console.log("listening on *:5000");
+  console.log("listening on *:5000");
 });
--- learntosocketio/public/javascripts/all.js
+++ learntosocketio/public/javascripts/all.js
@@ -1,9 +1,15 @@
 // Make connection
-//   socket.on('news', function (data) {
-    //     console.log(data);
-    //     socket.emit('my other event', { my: 'data' })
-    
-    $(()=>{
-        var socket = io.connect('http://localhost:5000');
+var socket = io.connect("http://localhost:5000");
 
-})
+// Client send button
+$("#userA").click(function() {
+  console.log("userA");
+  socket.emit("Client-send-data", "This is a message to you.");
+});
+
+// Client receive what server sends
+socket.on("Server-send-data", function(msg) {
+  $("#server-return-msg").append(
+    "What client receives from server-return: " + msg
+  );
+});
