02f1fd009f5fb1f738e58397c3fdddd9c74a2331
huongnguyen2152360 <huongnguyen2152360@gmail.com>
2018-10-04 19:42:20 +0700

    done layout

--- chessbyhuong/bin/socket.js
+++ chessbyhuong/bin/socket.js
@@ -1,9 +1,21 @@
 const app = require("../app");
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+const allUsernames = ["aaa"];
 
 io.on("connection", function(socket) {
   console.log(socket.id + " is connected.");
+  // Nhận đăng ký username
+  socket.on("client--regisUsername", function(username) {
+    // Đăng ký fail
+    if (allUsernames.indexOf(username) >= 0) {
+      socket.emit("server--regisFail");
+    } else {
+      allUsernames.push(username);
+      socket.emit("server--regisSuccess", username);
+      io.sockets.emit("server--usersOnline", allUsernames);
+    }
+  });
 });
 
 http.listen(5000, function() {
--- chessbyhuong/public/javascripts/all.js
+++ chessbyhuong/public/javascripts/all.js
@@ -1,1 +1,28 @@
 var socket = io.connect("http://localhost:5000");
+$(".game-room").hide();
+// Client đăng ký username
+$("#register-input").keypress(function(e) {
+  let key = e.which;
+  if (key == 13) {
+    // the enter key code
+    $("#register-btn").click();
+    socket.emit("client--regisUsername", $("#register-input").val());
+  }
+});
+
+// Nhận kết quả đăng ký username
+// Đăng ký failed
+socket.on("server--regisFail", function() {
+  $(".regis-notice").html(`<h5>Username existed.</h5>`);
+});
+// Đăng ký success
+socket.on("server--regisSuccess", function(username) {
+  $(".container").hide(3000);
+  $(".game-room").show(3000);
+  $(".chat--username").html(username);
+});
+
+// Hiện users online
+socket.on("server--usersOnline", function(usernames) {
+    
+})
