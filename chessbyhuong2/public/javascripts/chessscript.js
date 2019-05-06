951861c8f4af67feea30e68f3c603a87fc408d18
Hương Nguyễn <huongnguyen2152360@gmail.com>
2019-05-06 14:08:51 +0700

    new chess

---
+++ chessbyhuong2/public/javascripts/chessscript.js
@@ -0,0 +1,9 @@
+const cfg = {
+    pieceTheme: 'images/chesspieces/wikipedia/{piece}.png',
+    position: 'start',
+    showNotation: false,
+    draggable: true,
+  dropOffBoard: 'snapback',
+};
+const board1 = ChessBoard('board1', cfg);
+$(window).resize(board.resize);
